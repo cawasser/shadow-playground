e466aa59764a8072caa0844c8e2b38a1f41d7792
Chris Wasser <cawasser@mac.com>
2020-03-25 09:57:50 -0400

    basic updates to get some JS interop. :as requires working properly, :refer requires are NOT (react.createElement fails)

--- src/main.cljs
+++ src/main.cljs
@@ -1,19 +1,25 @@
 (ns main
   (:require [reagent.core :as r]
             [cljs.core.async :refer (chan put! <! go go-loop timeout)]
-            ))
+            ["@material-ui/core/Button" :refer [Button]]
+            ["toastr" :as toastr]
+            ["worldwind-react-globe" :as Globe]))
 
 
 
 (defn main-component []
-  [:div 
+  [:div
    [:h1 "This is a component"]
-   ])
+   [:> Button {:on-click #(toastr/success "Toast!")} "Button!"]
+   [:> Globe {:layers ["usgs-topo"
+                       "coordinates"
+                       "view-controls"
+                       "compass"]}]])
 
 
 (defn mount [c]
-  (r/render-component [c] (.getElementById js/document "app"))
-  )
+  (r/render-component [c] (.getElementById js/document "app")))
+
 
 (defn reload! []
   (mount main-component)
