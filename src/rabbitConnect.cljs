62286292eaf7b7de07750d45d8e3520178105f78
George Pierce <gpierce3@hotmail.com>
2020-04-08 11:14:32 -0400

    first crack at trying to connect to rabbit.  Not working.

--- src/rabbitConnect.cljs
+++ src/rabbitConnect.cljs
@@ -1,7 +1,13 @@
 (ns rabbitConnect
-  (:require ["toastr" :as toastr]))
+  (:require ["toastr" :as toastr]
+            ["rhea" :as rhea]))
+
+(def connectionOptions {:host "127.0.0.1" :port 5672 :username "guest" :password "guest"})
 
 (defn connect-to-broker []
-  (toastr/success "Connecting to RabbitMQ!"))
+  (toastr/success "Connecting to RabbitMQ!")
+  (rhea/connect connectionOptions))
+
+
 
 
--- src/rabbitPublish.cljs
+++ src/rabbitPublish.cljs
@@ -1,7 +1,9 @@
 (ns rabbitPublish
-  (:require ["toastr" :as toastr]))
+  (:require ["toastr" :as toastr]
+            [rabbitConnect :as rc]))
 
 (defn publish-message []
-  (toastr/success "Publishing to RabbitMQ!"))
+  (toastr/success "Publishing to RabbitMQ!")
+  (rc/connect-to-broker))
 
 
