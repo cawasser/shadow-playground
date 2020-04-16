bfba99105582f0ec220c70df3220dab0e4a83318
George Pierce <gpierce3@hotmail.com>
2020-04-16 15:13:24 -0400

    still experimenting

--- src/rabbitConnect.cljs
+++ src/rabbitConnect.cljs
@@ -3,11 +3,15 @@
             ["rhea" :as rhea]))
 
 (def connectionOptions {:host "127.0.0.1" :port 5672 :username "guest" :password "guest" :transport "tcp"})
+(def conn (rhea/create_connection (clj->js connectionOptions)))
 
 (defn connect-to-broker []
 
   (try
-    (rhea/connect (clj->js connectionOptions))
+    (rhea/on (clj->js "sendable") (fn [context] (toastr/success "sendable")))
+    (toastr/success "rhea on worked!")
+    (.connect conn)
+    ;(rhea/create_connection (clj->js connectionOptions))
     (toastr/success "success")
     true
 
